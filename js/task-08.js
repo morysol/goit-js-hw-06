284d5e99d10c9b00806a58a54648b3212cf96586
morysol <cepera.cMupHoB@gmail.com>
2022-04-14 22:48:48 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = {
+    email: event.currentTarget.elements.email.value,
+    pass: event.currentTarget.elements.password.value,
+  };
+
+  if (formData.email === '' || formData.pass === '') {
+    alert('Все поля должны быть заполнены!');
+  } else {
+    console.log(formData);
+    form.reset();
+  }
+}
