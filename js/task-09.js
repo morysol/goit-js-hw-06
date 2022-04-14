d8654efbc78542824aa2893060fa269c2f23bb3a
morysol <cepera.cMupHoB@gmail.com>
2022-04-14 23:05:50 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const btnChangeColor = document.querySelector('button.change-color');
+const bodyEl = document.querySelector('body');
+const colorValueUI = document.querySelector('span.color');
+
+btnChangeColor.addEventListener('click', () => {
+  colorValueUI.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = colorValueUI.textContent;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
