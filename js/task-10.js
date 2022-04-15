4adb9f8d56c138ed74be0e8172807f01e1551335
morysol <cepera.cMupHoB@gmail.com>
2022-04-15 08:17:21 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
+const controls = {
+  inputAmount: document.querySelector('#controls input'),
+  boxesUI: document.querySelector('#boxes'),
+  btnCreate: document.querySelector('button[data-create]'),
+  btnDestroy: document.querySelector('button[data-destroy]'),
+};
+
+function getInputAmount(params) {
+  return document.querySelector('#controls input').value;
+}
+
+controls.btnCreate.addEventListener('click', () => {
+  controls.boxesUI.insertAdjacentHTML('afterbegin', makeBoxesMarkup(getInputAmount()));
+});
+controls.btnDestroy.addEventListener('click', () => {
+  controls.boxesUI.innerHTML = '';
+});
+
+function makeBoxesMarkup(counter) {
+  const limit = counter;
+  const boxSize = 30;
+
+  let result = '';
+
+  for (let i = 0; i < limit; i += 1) {
+    result = `<div style="width: ${boxSize + i * 10}px;
+   height: ${boxSize + i * 10}px; background-color: ${getRandomHexColor()}">${result}
+  </div>`;
+  }
+
+  return result;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
